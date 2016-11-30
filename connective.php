d09143fd9a669ce0d46a6f9b04ce943f1b1c2b11
Jackie <reh.jackie@gmail.com>
2016-11-29 23:48:08 -0500

    fixing more

--- connective.php
+++ connective.php
@@ -3,7 +3,7 @@ define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
 define('DB_NAME', 'ad_bb2de57421bd8ae');
 define('DB_USER','b272612f8dfd45');
 define('DB_PASSWORD','2c9bc5a5');
-defint('DB_DATABASE', 'Dedection-Database')
+define('DB_DATABASE', 'Dedection-Database')
 
 //$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE) or die("Failed to connect to MySQL: " . mysql_error());
 //$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
