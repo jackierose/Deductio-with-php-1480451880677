572380d877bafd66eec213467941e549fc42a833
Jackie <reh.jackie@gmail.com>
2016-11-30 12:20:34 -0500

    just the text

--- connective.php
+++ connective.php
@@ -12,7 +12,6 @@ function SignIn() {
     $ID = $_POST['uname'];
     $Password = $_POST['pwd'];
     $sql = "select * from user_table where user_name = '$ID' and password_id = '$Password'";
-    $query = mysql_db_query("$sql") or die(mysql_error());
     echo $_POST['uname'];
   }
 }
