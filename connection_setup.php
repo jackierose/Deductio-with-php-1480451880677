132678001e9e23414ab82e547244a03a9e15df7c
Jackie <reh.jackie@gmail.com>
2016-11-30 13:54:01 -0500

    it helps when it knows

--- connection_setup.php
+++ connection_setup.php
@@ -1,9 +1,9 @@
 <?php
-/*define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
 define('DB_NAME', 'ad_bb2de57421bd8ae');
 define('DB_USER','b272612f8dfd45');
 define('DB_PASSWORD','2c9bc5a5');
-define('DB_DATABASE', 'Deductio-Database');*/
+define('DB_DATABASE', 'Deductio-Database');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
 or die("Failed to connect to MySQL: " . mysql_error());
