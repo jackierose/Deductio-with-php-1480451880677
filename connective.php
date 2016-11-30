dde74f088a54bb2a72df93029d8eecab59b8a45e
Jackie <reh.jackie@gmail.com>
2016-11-29 22:48:10 -0500

    replacing session_start

--- connective.php
+++ connective.php
@@ -7,12 +7,13 @@ define('DB_PASSWORD','2c9bc5a5');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
+session_start();
 /*
 $ID = $_POST['uname']]; //grabbing user name login
 $Password = $_POST['pwd']; //grabbing password from login
 */
 function SignIn() {
-   // this will start session for user profile page
+  //session_start(); // this will start session for user profile page
   //echo "SESSION STARTED";
   if(!empty($_REQUEST['uname'])) { //is there a user name in the box??
     echo "THE USER NAME WAS NOT EMPTY!";
