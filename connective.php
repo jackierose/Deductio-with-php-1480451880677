a35155c4da23e2657955377c3f581b1101ff455c
Jackie <reh.jackie@gmail.com>
2016-11-29 23:21:01 -0500

    is this the problem?

--- connective.php
+++ connective.php
@@ -7,7 +7,7 @@ define('DB_PASSWORD','2c9bc5a5');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
-session_start();
+//session_start();
 /*
 $ID = $_POST['uname']]; //grabbing user name login
 $Password = $_POST['pwd']; //grabbing password from login
