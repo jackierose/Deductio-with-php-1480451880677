816832eb578396dd3bbbc165707531bdd06b0f58
Jackie <reh.jackie@gmail.com>
2016-11-29 21:15:15 -0500

    Adding Description Page
    file transfer

---
+++ descriptionpage/descriptionpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Description Page</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<h1 class = "header">Deductio </h1>
+	<p class= "description">To play, move your user using the arrow keys from the start to the end. Use your deduction skills.</p>
+	<div class="buttonHolder">
+		<button class="start-button"><a href = "../level1/level1.html">Start</a></button>
+	</div>
+</body>
+
+
+</html>
