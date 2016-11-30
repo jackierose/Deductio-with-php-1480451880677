6c511b6bc95ca38f78f6ad91397e66b58c082379
Jackie <reh.jackie@gmail.com>
2016-11-29 23:32:53 -0500

    I don't know anymore

--- connective.php
+++ connective.php
@@ -7,12 +7,8 @@ define('DB_PASSWORD','2c9bc5a5');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
-//session_start();
-/*
-$ID = $_POST['uname']]; //grabbing user name login
-$Password = $_POST['pwd']; //grabbing password from login
-*/
-function SignIn() {
+
+//function SignIn() {
   //session_start(); // this will start session for user profile page
   //echo "SESSION STARTED";
   //if(!empty($_REQUEST['uname'])) { //is there a user name in the box??
@@ -29,9 +25,9 @@ function SignIn() {
       echo "ENTERED INCORRECT USERNAME OR PASSWORD";
     }*/
   //}
-}
-if($_SERVER["REQUEST_METHOD"] == "POST") {//isset($_REQUEST['submit'])) {
-  echo "JUST FUCKING PRINT OUT SOMETHING";
+//}
+if(isset($_POST['submit'])) {
+  echo "GO GLEN COCO!";
 	//SignIn();
 }
  ?>
