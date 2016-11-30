fbb256da813958132cc5f43f7fccec6e8bed920e
Jackie <reh.jackie@gmail.com>
2016-11-30 13:16:14 -0500

    got rid of old stuff

--- connective.php
+++ connective.php
@@ -1,13 +1,6 @@
 <?php
-define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
-define('DB_NAME', 'ad_bb2de57421bd8ae');
-define('DB_USER','b272612f8dfd45');
-define('DB_PASSWORD','2c9bc5a5');
-define('DB_DATABASE', 'Deductio-Database');
 
-$con=mysql_connect(ini_get(DB_HOST),ini_get(DB_USER),ini_get(DB_PASSWORD))
-or die("Failed to connect to MySQL: " . mysql_error());
-
+include("connection_setup.php");
 session_start();
 
 function SignIn() {
