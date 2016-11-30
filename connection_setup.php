d6067a58f09d859c6f423d8291c7b1330cc7d79f
Jackie <reh.jackie@gmail.com>
2016-11-30 13:15:52 -0500

    Tried new way on connecting.

---
+++ connection_setup.php
@@ -0,0 +1,26 @@
+<?php
+/*define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_NAME', 'ad_bb2de57421bd8ae');
+define('DB_USER','b272612f8dfd45');
+define('DB_PASSWORD','2c9bc5a5');
+define('DB_DATABASE', 'Deductio-Database');*/
+
+//$con=mysql_connect(ini_get(DB_HOST),ini_get(DB_USER),ini_get(DB_PASSWORD))
+//or die("Failed to connect to MySQL: " . mysql_error());
+
+$sqldb_config = $services_json["sqldb"][0]["credentials"];
+
+// create DB connect string
+$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};";
+$conn_string .= "DATABASE=" . $sqldb_config["Deductio-Database"] . ";";
+$conn_string .= "HOSTNAME=" . $sqldb_config["us-cdbr-iron-east-04.cleardb.net"] . ";";
+$conn_string .= "PROTOCOL=TCPIP;";
+$conn_string .= "UID=" . $sqldb_config["b272612f8dfd45"] . ";";
+$conn_string .= "PWD=" . $sqldb_config["2c9bc5a5"] . ";";
+
+// connect to database
+$conn = db2_connect($conn_string, '', '');
+
+
+
+ ?>
