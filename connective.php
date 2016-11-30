b41b18a13d2c392e2f90072291bf06922e6fc3cd
Jackie <reh.jackie@gmail.com>
2016-11-29 23:47:43 -0500

    wrong syntax

--- connective.php
+++ connective.php
@@ -3,13 +3,13 @@ define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
 define('DB_NAME', 'ad_bb2de57421bd8ae');
 define('DB_USER','b272612f8dfd45');
 define('DB_PASSWORD','2c9bc5a5');
+defint('DB_DATABASE', 'Dedection-Database')
 
-//$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+//$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE) or die("Failed to connect to MySQL: " . mysql_error());
 //$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 session_start();
 
 function SignIn() {
-  session_start(); // this will start session for user profile page
   if(!empty($_POST['uname'])) { //is there a user name in the box??
     echo "THE USER NAME WAS NOT EMPTY!";
     /*$ID = $_POST['uname']];
@@ -23,8 +23,8 @@ function SignIn() {
     } else {
       echo "ENTERED INCORRECT USERNAME OR PASSWORD";
     }*/
-  //}
-//}
+  }
+}
 if(isset($_POST['submit'])) {
 	SignIn();
 }
