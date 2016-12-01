4be50743bc84346cc25eb2042f52e7f9a5a7ce80
Jackie <reh.jackie@gmail.com>
2016-12-01 14:09:45 -0500

    again and again

--- connection_setup.php
+++ connection_setup.php
@@ -6,16 +6,12 @@ $password = '2c9bc5a5';
 $hostname = 'us-cdbr-iron-east-04.cleardb.net';
 $port = 3306;
 
-$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=$database;" .
-  "HOSTNAME=$hostname;PORT=$port;PROTOCOL=TCPIP;UID=$user;PWD=$password;";
-$conn = db2_connect($conn_string, '', '');
-
-if ($conn) {
-    echo "Connection succeeded.";
-    db2_close($conn);
-}
-else {
-    echo "Connection failed.";
+$con=mysqli_init();
+if (!$con)
+  {
+  echo "no";
+} else {
+  echo "yes";
 }
 
  ?>
