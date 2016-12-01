2c32b13633168c4bc52bc1ea6bd51ee38dd7a260
Jackie <reh.jackie@gmail.com>
2016-12-01 16:06:18 -0500

    set up again

--- connection_setup.php
+++ connection_setup.php
@@ -6,12 +6,21 @@ $password = '2c9bc5a5';
 $hostname = 'us-cdbr-iron-east-04.cleardb.net';
 $port = 3306;
 
-$db = new mysqli($hostname,$user,$password,$database) or die("Unable to connect");
-
-echo "Good Job!";
-
-
+$services = getenv("VCAP_SERVICES"); // searches for environment variables needed to fetch the info for data base
+$services_json = json_decode($services,true); // decodes the variables that were fetched
+$mysql_config = $services_json["mysql-5.5"][0]["credentials"]; //sets up the servcie config
+echo services;
 /*
+// sets up data base stuff
+$db = $mysql_config["ad_bb2de57421bd8ae"];
+$host = $mysql_config["us-cdbr-iron-east-04.cleardb.net"];
+$port = $mysql_config["3306"];
+$username = $mysql_config["b272612f8dfd45"];
+$password = $mysql_config["2c9bc5a5"];
+
+//tries to connect
+$conn = mysql_connect($host, $username, $password);
+
 if (!$con)
   {
   echo "no";
