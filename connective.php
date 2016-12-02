f8f7a6654877c54fe840a9168f8b2092b730ce85
Jackie <reh.jackie@gmail.com>
2016-12-02 12:50:34 -0500

    evannnn

--- connective.php
+++ connective.php
@@ -7,11 +7,12 @@ function SignIn() {
   if(!empty($_POST['uname'])) {
     $ID = $_POST['uname'];
     $Password = $_POST['psw'];
-    $myQuery = "SELECT uname where password_id = '$Password' FROM user_table;"; //WHERE user_name = '$ID'AND password_id = '$Password'";
+    $myQuery = "SELECT * FROM user_table"; //WHERE user_name = '$ID'AND password_id = '$Password'";
     $qr = mysqli_query($con,$myQuery);
     $row=mysqli_fetch_array($qr,MYSQLI_ASSOC);
 
-    var_dump($qr);
+    echo $qr;
+    //var_dump($qr);
   //  var_dump($myQuery);
 
 /*
