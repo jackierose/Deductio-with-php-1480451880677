dd657fb109002e223e49d927277121833eb3440d
Jackie <reh.jackie@gmail.com>
2016-12-02 13:24:32 -0500

    ugher

--- connective.php
+++ connective.php
@@ -1,7 +1,16 @@
 <?php
 
-include("connection_setup.php");
 session_start();
+$services = getenv("VCAP_SERVICES"); // searches for environment variables needed to fetch the info for data base
+$services_json = json_decode($services,true); // decodes the variables that were fetched
+$mysql_config = $services_json["cleardb"][0]["credentials"]; //sets up the servcie config
+
+$db = $mysql_config["name"];
+$host = $mysql_config["hostname"];
+$port = $mysql_config["port"];
+$username = $mysql_config["username"];
+$password = $mysql_config["password"];
+
 $con = mysqli_connect($host,$username,$password,$db);
 
 function SignIn() {
