278f8355ca2b4bddcd6a41c9ee49af4b3616c404
Jackie <reh.jackie@gmail.com>
2016-12-02 13:21:06 -0500

    connect plz

--- connection_setup.php
+++ connection_setup.php
@@ -1,4 +1,5 @@
 <?php
+include("connective.php");
 
 $services = getenv("VCAP_SERVICES"); // searches for environment variables needed to fetch the info for data base
 $services_json = json_decode($services,true); // decodes the variables that were fetched
@@ -12,25 +13,7 @@ $port = $mysql_config["port"];
 $username = $mysql_config["username"];
 $password = $mysql_config["password"];
 
-$con = mysqli_connect($host,$username,$password,$db);
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
-  $result = mysqli_query($con, "SELECT * FROM user_table");
-
-  $return_arr = array();
-
-  while ($row = mysqli_fetch_array($result))
-  {
-      $return_arr[] = array(
-          'name' => $row['uname'],
-          'pass' => $row['password_id']
-      );
-  }
-
-  echo json_encode($return_arr);
 
 
 
--- connective.php
+++ connective.php
@@ -2,27 +2,31 @@
 
 include("connection_setup.php");
 session_start();
+$con = mysqli_connect($host,$username,$password,$db);
 
 function SignIn() {
   if(!empty($_POST['uname'])) {
     $ID = $_POST['uname'];
     $Password = $_POST['psw'];
-    //$myQuery = "SELECT * FROM user_table"; //WHERE user_name = '$ID'AND password_id = '$Password'";
-    //$qr = mysqli_query($con,$myQuery);
-    //$row=mysqli_fetch_array($qr,MYSQLI_ASSOC);
 
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
 
-    //var_dump($qr);
-  //  var_dump($myQuery);
+      $result = mysqli_query($con, "SELECT * FROM user_table");
 
-/*
-    if(!empty($row['user_name']) AND !empty($row['password_id'])) {
-      $_SESSION['user_name'] = $row['password_id'];
-      echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
-    } else {
-      echo "Wrong password";
-    }
-*/
+      $return_arr = array();
+
+      while ($row = mysqli_fetch_array($result))
+      {
+          $return_arr[] = array(
+              'name' => $row['user_name'],
+              'pass' => $row['password_id']
+          );
+      }
+
+      echo json_encode($return_arr);
   }
 }
 
