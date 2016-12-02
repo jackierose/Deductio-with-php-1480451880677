dccae262727e8034659c2c8a51451f51e217e756
Jackie <reh.jackie@gmail.com>
2016-12-02 13:31:54 -0500

    reorganizing

--- connective.php
+++ connective.php
@@ -1,28 +1,28 @@
 <?php
 
 session_start();
-$services = getenv("VCAP_SERVICES"); // searches for environment variables needed to fetch the info for data base
-$services_json = json_decode($services,true); // decodes the variables that were fetched
-$mysql_config = $services_json["cleardb"][0]["credentials"]; //sets up the servcie config
-
-$db = $mysql_config["name"];
-$host = $mysql_config["hostname"];
-$port = $mysql_config["port"];
-$username = $mysql_config["username"];
-$password = $mysql_config["password"];
-
-
 
 function SignIn() {
+  $services = getenv("VCAP_SERVICES"); // searches for environment variables needed to fetch the info for data base
+  $services_json = json_decode($services,true); // decodes the variables that were fetched
+  $mysql_config = $services_json["cleardb"][0]["credentials"]; //sets up the servcie config
+
+  $db = $mysql_config["name"];
+  $host = $mysql_config["hostname"];
+  $port = $mysql_config["port"];
+  $username = $mysql_config["username"];
+  $password = $mysql_config["password"];
+
+  $con = mysqli_connect($host,$username,$password,$db);
+  if (mysqli_connect_errno())
+    {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
   if(!empty($_POST['uname'])) {
     $ID = $_POST['uname'];
     $Password = $_POST['psw'];
 
-    if (mysqli_connect_errno())
-      {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-      }
-      $con = mysqli_connect($host,$username,$password,$db);
       $result = mysqli_query($con, "SELECT * FROM user_table");
 
       $return_arr = array();
