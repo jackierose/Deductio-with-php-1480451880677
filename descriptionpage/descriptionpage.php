98508cd2b0e3ab0d5dd334635032abe525fa5e74
Jackie <reh.jackie@gmail.com>
2016-12-02 21:52:06 -0500

    Added and updated

--- descriptionpage/descriptionpage.php
+++ descriptionpage/descriptionpage.php
@@ -6,10 +6,15 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<h1 class = "header">Deductio </h1>
-	<p class= "description">To play, move your user using the arrow keys from the start to the end. Use your deduction skills.</p>
-	<div class="buttonHolder">
-		<button class="start-button"><a href = "../level1/level1.html">Start</a></button>
+	<h1 class = "header">DEDUCTIO </h1>
+	<div class = "mainHolder">
+		<p class= "description">Using the arrow keys, maneuver from start to end. Use your deduction skills.</p>
+		<div class="buttonHolder">
+			<button class="start-button" onclick = "location.href='../level1/level1.html'" type = button>Start</button>
+		</div>
+	</div>
+	<div class = "logodiv">
+		<img class = "logo" alt = "logo image" src='../images/brainy.png'>
 	</div>
 </body>
 
