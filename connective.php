c46a3262465685be251c71d57b36cac56c69a5f0
Jackie <reh.jackie@gmail.com>
2016-12-02 21:32:33 -0500

    okay

--- connective.php
+++ connective.php
@@ -25,12 +25,13 @@ function SignIn() {
 
       $result = mysqli_query($con, "SELECT * FROM user_table");
 
-      $row = mysqli_fetch_array($result)
+echo "okay";
+    /*  $row = mysqli_fetch_array($result)
 
       if(!empty($row['user_name']) AND !empty($row['password_id'])) {
         $_SESSION['userName'] = $row['pass'];
         echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
-      }
+      } */
 
   }
 }
