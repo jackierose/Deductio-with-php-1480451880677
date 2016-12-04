9c163eaeb53958622cdfcdc097a32e392e27d561
Jackie <reh.jackie@gmail.com>
2016-12-04 11:57:20 -0500

    Rename files endings to php

--- descriptionpage/descriptionpage.php
+++ descriptionpage/descriptionpage.php
@@ -1,3 +1,4 @@
+session_start();
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,11 +7,11 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<h1 class = "header">DEDUCTIO </h1>
+	<h1 class = "header">$_SESSION['user_name'] </h1>
 	<div class = "mainHolder">
 		<p class= "description">Using the arrow keys, maneuver from start to end. Use your deduction skills.</p>
 		<div class="buttonHolder">
-			<button class="start-button" onclick = "location.href='../introlevel/introlevel.html'" type = button>Start</button>
+			<button class="start-button" onclick = "location.href='../introlevel/introlevel.php'" type = button>Start</button>
 		</div>
 	</div>
 	<div class = "logodiv">
--- introlevel/introlevel.php
+++ introlevel/introlevel.php
@@ -1,4 +1,24 @@
-<?php
 session_start();
-echo "session = {$_SESSION["user_name"]} <BR />";
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Intro Level</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<h1 class = "header">Deductio </h1>
+	<canvas class="canvas" id="myCanvas" width="460" height="460"></canvas>
+	<div class="buttonHolder">
+		<button class="start-button" onclick = "location.href='../descriptionpage/descriptionpage.html'" type = button>Exit</button>
+	</div>
+	<div class="buttonHolder">
+		<button class="start-button" onclick = "restart();" type = button>Restart</button>
+	</div>
+	<div class="buttonHolder">
+		<button class="start-button" id = "nextButton" style="visibility:hidden;" onclick = "location.href='../level0/level0.html'" type = button>Next Level</button>
+	</div>
+
+    <script type="text/javascript" src="introlevel.js"></script>
+</body>
+</html>
