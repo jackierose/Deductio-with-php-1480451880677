2d6f8420a6c2063d923a23900ee6ab218c4dd672
Jackie <reh.jackie@gmail.com>
2016-12-04 13:39:06 -0500

    plz

--- descriptionpage/descriptionpage.php
+++ descriptionpage/descriptionpage.php
@@ -16,7 +16,7 @@ if (mysqli_connect_errno())
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-echo "SELECT * FROM user_table WHERE user_name = '$_SESSION['user_name']'";
+//echo "SELECT * FROM user_table WHERE user_name = $_SESSION['user_name']";
 	//$result = mysqli_query($con, "SELECT * FROM user_table WHERE user_name = '$_SESSION['user_name']'");
 	//$row = mysqli_fetch_array($result);
 	/* if($row['level'] == NULL) {
